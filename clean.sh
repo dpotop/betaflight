91ef8e4b0ff85319375c486b360c5cabd8a66d37
Dumitru Potop Butucaru <dpotop@Dumitrus-MacBook-Pro.local>
2024-09-29 00:28:23 +0200

    Minor

--- clean.sh
+++ clean.sh
@@ -4,5 +4,8 @@ set -x
 
 find src -name *.o -exec rm -f {} \;
 find lib -name *.o -exec rm -f {} \;
+find src -name *.d -exec rm -f {} \;
+find lib -name *.d -exec rm -f {} \;
+
 
 rm -f betaflight_4.6.0_STM32F405_SPEEDYBEEF405V4.hex betaflight_STM32F405_SPEEDYBEEF405V4.elf betaflight_STM32F405_SPEEDYBEEF405V4.map
